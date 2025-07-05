3da04c657cdc531b17d47a00d5e665c15e14a907
Fatemeh Saeedi <fatemehsaeediofficial@gmail.com>
2025-07-05 13:01:59 +0330

    add Health Bar for Boss Fight

--- Assets/Scripts/Level3/BossHealthUI.cs
+++ Assets/Scripts/Level3/BossHealthUI.cs
@@ -3,10 +3,25 @@ using UnityEngine.UI;
 
 public class BossHealthUI : MonoBehaviour
 {
-    public Slider healthSlider;
+    public Image healthImage;
+
+    public Sprite health100;
+    public Sprite health75;
+    public Sprite health50;
+    public Sprite health25;
+    public Sprite health0;
 
     public void UpdateHealthBar(float healthPercent)
     {
-        healthSlider.value = healthPercent;
+        if (healthPercent >= 0.99f)
+            healthImage.sprite = health100;
+        else if (healthPercent >= 0.74f)
+            healthImage.sprite = health75;
+        else if (healthPercent >= 0.49f)
+            healthImage.sprite = health50;
+        else if (healthPercent >= 0.24f)
+            healthImage.sprite = health25;
+        else
+            healthImage.sprite = health0;
     }
 }
