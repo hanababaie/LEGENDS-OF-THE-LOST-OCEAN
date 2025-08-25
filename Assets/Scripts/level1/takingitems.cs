4af464f39c82509133ed7c7d9871fedbd02c9077
hana <hananebabaie1385@gmail.com>
2025-08-25 18:37:25 +0330

    Update takingitems.cs
    fixing taking items

--- Assets/Scripts/level1/takingitems.cs
+++ Assets/Scripts/level1/takingitems.cs
@@ -76,64 +76,64 @@ public class takingitems : MonoBehaviour
                 }
             }
 
-            if (p3 != null)
+            if (p3 != null) // player has p1offline
             {
                 switch (loot.lootname)
                 {
                     case "health":
-                        other.GetComponent<playermovement2>().addhealth(1);
+                        p3.addhealth(1);
                         break;
                     case "coins":
-                        other.GetComponent<playermovement2>().addcoin(50);
+                        p3.addcoin(50);
                         break;
                     case "speed":
-                        other.GetComponent<playermovement2>().addspeed(20);
+                        p3.addspeed(20);
                         break;
-                    case "Gold" :
-                        other.GetComponent<playermovement2>().addcoin(100);
+                    case "Gold":
+                        p3.addcoin(100);
                         break;
                     case "New Life":
-                        other.GetComponent<playermovement2>().addlives(1);
+                        p3.addlives(1);
                         break;
-                    case "Speed bosster" :
-                        other.GetComponent<playermovement2>().addspeed(35);
+                    case "Speed bosster":
+                        p3.addspeed(35);
                         break;
                     case "Healing":
-                        other.GetComponent<playermovement2>().addhealth(1);
+                        p3.addhealth(1);
                         break;
                     case "Power booster":
-                        other.GetComponent<playermovement2>().addpower(10);
+                        p3.addpower(10);
                         break;
                 }
             }
 
-            if (p4 != null)
+            if (p4 != null) // player has p2offline
             {
                 switch (loot.lootname)
                 {
                     case "health":
-                        other.GetComponent<playermovement2>().addhealth(1);
+                        p4.addhealth(1);
                         break;
                     case "coins":
-                        other.GetComponent<playermovement2>().addcoin(50);
+                        p4.addcoin(50);
                         break;
                     case "speed":
-                        other.GetComponent<playermovement2>().addspeed(20);
+                        p4.addspeed(20);
                         break;
-                    case "Gold" :
-                        other.GetComponent<playermovement2>().addcoin(100);
+                    case "Gold":
+                        p4.addcoin(100);
                         break;
                     case "New Life":
-                        other.GetComponent<playermovement2>().addlives(1);
+                        p4.addlives(1);
                         break;
-                    case "Speed bosster" :
-                        other.GetComponent<playermovement2>().addspeed(35);
+                    case "Speed bosster":
+                        p4.addspeed(35);
                         break;
                     case "Healing":
-                        other.GetComponent<playermovement2>().addhealth(1);
+                        p4.addhealth(1);
                         break;
                     case "Power booster":
-                        other.GetComponent<playermovement2>().addpower(10);
+                        p4.addpower(10);
                         break;
                 }
             }
